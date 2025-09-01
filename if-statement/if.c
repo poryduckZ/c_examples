f7903e801fa40f142899b6616146187f80951bf7
Goh Yong Jie <gohyongjie1112@gmail.com>
2025-09-01 22:34:55 +0800

    add if statement example

---
+++ if-statement/if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    if (x < 10) {
+        printf("too cold \n");
+    } else if (x == 10) {
+        printf("just right \n");
+    } else {
+        printf("too hot \n");
+    }
+}
