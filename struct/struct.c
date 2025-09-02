e04da9d99150369f10b7c4b4b625e9a5d420e2ab
Goh Yong Jie <gohyongjie1112@gmail.com>
2025-09-02 23:14:53 +0800

    add typedef to struct example

--- struct/struct.c
+++ struct/struct.c
@@ -4,14 +4,20 @@ struct Coordinate {
     int x;
     int y;
     int z;
+} typedef coordinate_t;
+
+coordinate_t new_coord(int x, int y, int z) {
+    coordinate_t coord = {
+        .x = x,
+        .y = y,
+        .z = z,
+    };
+
+    return coord;
 };
 
 int main() {
-    struct Coordinate coordinate = {
-        .x = 1,
-        .y = 2,
-        .z = 3
-    };
+    coordinate_t coordinate = new_coord(1, 2, 3);
 
     printf("x: %d\n", coordinate.x);
     printf("y: %d\n", coordinate.y);
