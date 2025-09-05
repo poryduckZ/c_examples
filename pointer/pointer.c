1885ee21c4bfcf99147323c8d26b969c3ab10e37
poryduckZ <gohyongjie1112@gmail.com>
2025-09-05 23:25:41 +0800

    add arrow operator example

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+typedef struct Coordinate {
+    int x;
+    int y;
+    int z;
+} coordinate_t;
+
 int main() {
     int i = 1;
     int size = sizeof(&i);
 
     printf("Size: %d\n", size);
+
+    int example = 32;
+    int *pointer_to_example = &example;
+    int value_at_pointer = *pointer_to_example;
     
+    coordinate_t point = {10, 20, 30};
+    coordinate_t *ptr_to_point = &point;
+    printf("X: %d\n", ptr_to_point->x);
+
     return 0;
 }
